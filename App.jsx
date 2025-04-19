199204e7ecb505f59e054fe6ecd1b7c059ec18d4
Mojapela <mojapelamojapela97@gmail.com>
2025-04-20 00:44:08 +0200

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem', backgroundColor: '#d0f0c0' }}>
+      <h1 style={{ color: '#555' }}>Nostalgia Clothing</h1>
+      <p>Welcome to our store in Thaba-Bosiu, Maseru, Lesotho.</p>
+      <p>Style meets comfort. Est. 2025.</p>
+    </div>
+  );
+};
+
+export default App;
